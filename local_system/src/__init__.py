345245aa09c5029861f50b6c64eb774f96c59d2a
kimseongyu <kskksk0925@naver.com>
2025-04-08 18:39:08 +0900

    feat: add router with default data

--- local_system/src/__init__.py
+++ local_system/src/__init__.py
@@ -1,3 +1,5 @@
+from .chatbot import ChatBot
 from .core import FacmanApplication
+from .core import OPENAI_API_KEY, BASE_DIR, VECTOR_DB_DIR, VECTOR_DB
 from .events import Event, EventGenerator, EventHandler
 from .utils import ThreadHandler
--- local_system/src/api/router.py
+++ local_system/src/api/router.py
@@ -1,8 +1,35 @@
-from fastapi import APIRouter
+from chatbot import ChatBot
+from fastapi import APIRouter, UploadFile
 
 router = APIRouter()
+chatbot = ChatBot()
 
+latest_file = ""
+latest_explain = ""
 
-@router.get("/")
-async def root():
-    return {"message": "Welcome to Facman AI"}
+@router.get("/problem_lists")
+async def problem_lists():
+    problems = [
+        {"id":"id1", "type": "type1", "value": "value1", "time": "2000.09.25"},
+        {"id":"id2", "type": "type2", "value": "value2", "time": "2000.09.25"},
+        {"id":"id3", "type": "type3", "value": "value3", "time": "2000.09.25"},
+        {"id":"id4", "type": "type4", "value": "value4", "time": "2000.09.25"},
+        {"id":"id5", "type": "type5", "value": "value5", "time": "2000.09.25"},
+    ]
+    return {"problems": problems}
+
+@router.post("/problem_complete")
+async def problem_complete(problem_id):
+    return {"problem_id":problem_id}
+
+@router.post("/solve_problem")
+async def solve_problem(problem_id, file: UploadFile, explain: str):
+    latest_file = file
+    latest_explain = explain
+    answer = chatbot.solve_problem("불이 났다.", file, explain)
+    return {"answer":answer}
+
+@router.get("/download_report/{problem_id}")
+async def get_report(problem_id: int):
+    answer = chatbot.make_report_content("불이 났다.", latest_file, latest_explain)
+    return {"answer":answer}
