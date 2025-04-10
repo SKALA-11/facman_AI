a2c043af10a3aac6c49e518d2f35b4bd9b76e0ee
kimseongyu <kskksk0925@naver.com>
2025-04-10 11:07:52 +0900

    feat: implement api each request

--- local_system/src/api/router.py
+++ local_system/src/api/router.py
@@ -1,35 +1,124 @@
-from chatbot import ChatBot
-from fastapi import APIRouter, UploadFile
+import base64
+from chatbot import chatbot
+from fastapi import APIRouter, UploadFile, Depends, HTTPException
+from sqlalchemy.ext.asyncio import AsyncSession
+from db.database import get_db
+from db.cruds import create_event, get_event, get_events
+from db.cruds import create_event_detail, get_event_detail, update_event_detail
+from db.cruds import (
+    create_solution,
+    get_solution,
+    update_solution,
+    update_solution_complete,
+)
+from typing import Optional
+from io import BytesIO
+from PIL import Image
 
 router = APIRouter()
-chatbot = ChatBot()
 
-latest_file = ""
-latest_explain = ""
 
-@router.get("/problem_lists")
-async def problem_lists():
-    problems = [
-        {"id":"id1", "type": "type1", "value": "value1", "time": "2000.09.25"},
-        {"id":"id2", "type": "type2", "value": "value2", "time": "2000.09.25"},
-        {"id":"id3", "type": "type3", "value": "value3", "time": "2000.09.25"},
-        {"id":"id4", "type": "type4", "value": "value4", "time": "2000.09.25"},
-        {"id":"id5", "type": "type5", "value": "value5", "time": "2000.09.25"},
-    ]
-    return {"problems": problems}
+@router.post("/create_event")
+async def create_event_router(
+    type: str, value: str, db: AsyncSession = Depends(get_db)
+):
+    event = await create_event(db, type, value)
+    return event
 
-@router.post("/problem_complete")
-async def problem_complete(problem_id):
-    return {"problem_id":problem_id}
 
-@router.post("/solve_problem")
-async def solve_problem(problem_id, file: UploadFile, explain: str):
-    latest_file = file
-    latest_explain = explain
-    answer = chatbot.solve_problem("불이 났다.", file, explain)
-    return {"answer":answer}
+@router.get("/event/{event_id}")
+async def get_event_router(event_id: int, db: AsyncSession = Depends(get_db)):
+    event = await get_event(db, event_id)
+    return {"event": event}
 
-@router.get("/download_report/{problem_id}")
-async def get_report(problem_id: int):
-    answer = chatbot.make_report_content("불이 났다.", latest_file, latest_explain)
-    return {"answer":answer}
+
+@router.get("/events")
+async def get_events_router(
+    skip: Optional[int] = 0,
+    limit: Optional[int] = 30,
+    db: AsyncSession = Depends(get_db),
+):
+    events = await get_events(db, skip, limit)
+    return {"events": events}
+
+
+@router.post("/solve_event")
+async def solve_event_router(
+    event_id: int, file: UploadFile, explain: str, db: AsyncSession = Depends(get_db)
+):
+    event = await get_event(db, event_id)
+    if not event:
+        raise HTTPException(
+            status_code=404, detail=f"Event with ID {event_id} not found"
+        )
+
+    bytes_data = await file.read()
+
+    img = Image.open(BytesIO(bytes_data))
+    img = img.convert("RGB")
+
+    max_size = (800, 800)
+    img.thumbnail(max_size, Image.LANCZOS)
+
+    buffer = BytesIO()
+    img.save(buffer, format="JPEG", quality=70)
+    compressed_bytes = buffer.getvalue()
+
+    file_encoded = base64.b64encode(compressed_bytes).decode("utf-8")
+
+    event_detail = await get_event_detail(db, event_id)
+
+    if event_detail:
+        event_detail = await update_event_detail(db, event_id, file_encoded, explain)
+    else:
+        event_detail = await create_event_detail(db, event_id, file_encoded, explain)
+
+    await file.seek(0)
+    answer = chatbot.solve_event(event, file_encoded, explain)
+
+    solution = await get_solution(db, event_id)
+    if solution:
+        solution = await update_solution(db, event_id, answer)
+    else:
+        solution = await create_solution(db, event_id, answer)
+
+    return {"event_id": event_id, "answer": answer}
+
+
+@router.post("/event_complete/{event_id}")
+async def event_complete_router(
+    event_id: int, complete: bool = True, db: AsyncSession = Depends(get_db)
+):
+    solution = await update_solution_complete(db, event_id, complete)
+    if not solution:
+        raise HTTPException(
+            status_code=404, detail=f"Solution with ID {event_id} not found"
+        )
+    return {"event_id": event_id, "complete": complete}
+
+
+@router.get("/download_report/{event_id}")
+async def get_event_report_router(event_id: int, db: AsyncSession = Depends(get_db)):
+    event = await get_event(db, event_id)
+    if not event:
+        raise HTTPException(
+            status_code=404, detail=f"Event with ID {event_id} not found"
+        )
+
+    event_detail = await get_event_detail(db, event_id)
+    if not event_detail:
+        raise HTTPException(
+            status_code=404, detail=f"Event detail for event ID {event_id} not found"
+        )
+
+    solution = await get_solution(db, event_id)
+    if not solution:
+        raise HTTPException(
+            status_code=404, detail=f"Solution for event ID {event_id} not found"
+        )
+
+    answer = chatbot.make_report_content(
+        event, event_detail.file, event_detail.explain, solution.answer
+    )
+
+    return {"answer": answer}
