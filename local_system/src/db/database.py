52ab2918900420a93ae8a4f91cac547dd32764a8
LeeSpect <wewqwew153@gmail.com>
2025-04-14 19:15:47 +0900

    feat: add database migration script to Dockerfile and update database connection string

--- local_system/src/db/database.py
+++ local_system/src/db/database.py
@@ -4,7 +4,9 @@ from sqlalchemy.orm import sessionmaker
 
 # MARIA_DB = "mysql+aiomysql://manager:SqlDba-1@127.0.0.1:3306/facman?charset=utf8mb4"
 
-MARIA_DB = "mysql+aiomysql://root:1234@localhost:3306/facman"
+# MARIA_DB = "mysql+aiomysql://root:1234@localhost:3306/facman"
+
+MARIA_DB = "mysql+aiomysql://root:skcc12345@facman.duckdns.org:3306/facman?charset=utf8"
 
 async_engine = create_async_engine(MARIA_DB, echo=True)
 
