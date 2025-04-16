2dd67b46131b231d02e37e051f4b86f49517f0fb
miiinn23 <ansnnd123@naver.com>
2025-04-16 23:14:57 +0900

    etc: main docs 추가(그냥해봤어요)

--- headquater_system/main.py
+++ headquater_system/main.py
@@ -14,6 +14,7 @@ app = FastAPI(
     title="FacMan AI HQ System",
     description="STT, 번역, TTS 기능을 제공하는 API 시스템",
     version="1.0.0",
+    docs_url='/ai/hq/docs'
 )
 
 # CORS 설정: 실제 배포 환경에 맞게 Vue.js 도메인 지정 필요
